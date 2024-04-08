eecb04722818563c048609c6b92417470cbbaa2b
0n3m0r3 <louka.altdorfreynes@gmail.com>
2024-04-08 15:24:03 +0200

    redirection

--- next.config.js
+++ next.config.js
@@ -1,25 +1,30 @@
-require('dotenv-flow').config({
-  node_env: process.env.APP_ENV || process.env.NODE_ENV || 'development',
-  silent: true
+require("dotenv-flow").config({
+  node_env: process.env.APP_ENV || process.env.NODE_ENV || "development",
+  silent: true,
 });
 
 /** @type {import('next').NextConfig} */
-module.exports ={
+module.exports = {
   reactStrictMode: true,
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: true
+    ignoreBuildErrors: true,
   },
   async redirects() {
     return [
+      // {
+      //   source: '/',
+      //   destination: '/reference-direct-payment-api',
+      //   permanent: false
+      // }
       {
-        source: '/',
-        destination: '/reference-direct-payment-api',
-        permanent: false
-      }
+        source: "/(.*)",
+        destination: "https://ribpay-docs.vercel.app/docs/ribpay-api/intro",
+        permanent: true,
+      },
     ];
   },
   async headers() {
@@ -31,7 +36,11 @@ module.exports ={
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
         ],
       },
     ];
